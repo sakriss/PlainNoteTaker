56968bf4acb1d71e50a9b11941dc4dd405573ac9
Scott Kriss <skriss@deloitte.com>
2017-05-02 10:10:43 -0700

    added > to each cell of the tableview to indicate it's a tappable cell

--- PlainNoteTaker/ViewController.swift
+++ PlainNoteTaker/ViewController.swift
@@ -63,6 +63,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell")!
         cell.textLabel?.text = data[indexPath.row]
+        cell.accessoryType = .disclosureIndicator
         return cell
     }
     
